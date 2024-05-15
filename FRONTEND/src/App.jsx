796632f8c5752245a84b826f21af0855be2305d4
EDS <n3.edson@hotmail.com>
2024-05-15 09:42:11 -0500

    Inciando proyecto con el frontend

---
+++ FRONTEND/src/App.jsx
@@ -0,0 +1,17 @@
+
+
+function App() {
+  
+
+  return (
+    <>
+      <div className='container'>
+        <h1>Hola React y Boostrap</h1>
+        <div className='btyn bt-outline-primary'>Registrar</div>
+        <div className='btyn bt-outline-primary'>Listar</div>
+      </div>
+    </>
+  )
+}
+
+export default App
